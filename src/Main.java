f5697f16c8266688cb64ba2cb59604ed6af7303d
DrVeronn <artyom.varapaev.jazzteam@gmail.com>
2021-08-12 11:36:05 +0300

    kjnoijjkm

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        print();
+    }
+    public static void print(){
+        System.out.println("ha-ha");
+    }
+}
